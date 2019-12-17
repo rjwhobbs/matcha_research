1ec68085f845b6c16449f137fc076bf03249c9d1
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-17 14:31:03 +0200

    What am I even commiting

--- signin.js
+++ signin.js
@@ -1,7 +1,7 @@
 const uuid = require('uuid/v4');
 const _ = require('lodash');
 const express = require('express');
-//const session = require('express-session');
+const session = require('express-session');
 let conn = require('./mysqlDB/conn');
 let sql = require('./mysqlDB/statements');
 
