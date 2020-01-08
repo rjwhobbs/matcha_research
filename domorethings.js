05cb6b7a8f1e66bbe6ae3e0026cb962fb355e693
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2020-01-08 09:02:24 +0200

    Can't update a foreign key from a child

--- domorethings.js
+++ domorethings.js
@@ -8,12 +8,14 @@ let sql = require('./mysqlDB/statements');
 let router = express.Router();
 module.exports = router;
 
-let testSql = 'INSERT INTO testusers (`username`, `email`, `password`) VALUES (?, ?, ?)';
+let testSql1 = 'INSERT INTO testusers (`username`, `email`, `password`) VALUES (?, ?, ?)';
 let testSql2 = 'INSERT INTO testtags (`user_id`, `tag`, `username`) VALUES (?, ?, ?)';
 
+let arr1 = ['Derp', 'derp@derp.com', 'derp'];
+let arr2 = [ 2, "derpy", "Derp"];
 
 router.get('/', function(req, res) {
-	conn.query(testSql2, [ 1, "derpy", "Derp"], function(err, results, feilds) {
+	conn.query(testSql2, arr2, function(err, results, feilds) {
 		if (err) {throw err}
 		console.log(results);
 		console.log(feilds);
