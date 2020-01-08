67355fe84b57fab17855109c6cdf226be4c5a526
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2020-01-08 09:35:49 +0200

    Doing more than 2 joins

--- domorethings.js
+++ domorethings.js
@@ -10,15 +10,28 @@ module.exports = router;
 
 let testSql1 = 'INSERT INTO testusers (`username`, `email`, `password`) VALUES (?, ?, ?)';
 let testSql2 = 'INSERT INTO testtags (`user_id`, `tag`, `username`) VALUES (?, ?, ?)';
+let testSql3 = 'INSERT INTO testmessages (`user_id`, `message`, `username`) VALUES (?, ?, ?)';
+let doTheJoins = "SELECT testusers.username, testtags.tag FROM testusers INNER JOIN testtags ON testusers.user_id = testtags.user_id";
+let doTheJoins2 = "SELECT testusers.username, testtags.tag " + 
+					"FROM testusers " + 
+					"INNER JOIN testtags " +
+					"ON testusers.user_id = ?";
+let doTheJoins4 = "SELECT testusers.username, testtags.tag, testmessages.message " + 
+					"FROM testusers " + 
+					"INNER JOIN testtags " +
+					"INNER JOIN testmessages " +
+					"ON testusers.user_id = ?";
+let doTheJoins3 = "SELECT testusers.username, testusers.email, testtags.tag FROM testusers INNER JOIN testtags ON testusers.user_id = ?";
 
 let arr1 = ['Derp', 'derp@derp.com', 'derp'];
-let arr2 = [ 2, "derpy", "Derp"];
+let arr2 = [2, "this is derpy message", "Derp"];
+let arr3 = [2];
 
 router.get('/', function(req, res) {
-	conn.query(testSql2, arr2, function(err, results, feilds) {
+	conn.query(doTheJoins4, arr3, function(err, results, feilds) {
 		if (err) {throw err}
 		console.log(results);
-		console.log(feilds);
+		//console.log(feilds);
 		res.send("Welcome to the test page");
 	});
 });
